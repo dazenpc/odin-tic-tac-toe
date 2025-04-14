b65bdeedc6468c2e2b0932e303a109a4735dee83
dazenpc <121750352+dazenpc@users.noreply.github.com>
2025-04-14 18:35:06 +0530

    fix: code cleanup and structuring

--- scripts.js
+++ scripts.js
@@ -1,89 +1,111 @@
-let gameBoard = [
-    -1,-1,-1,
-    -1,-1,-1,
-    -1,-1,-1
-]
+// Global variables
+let winningPlayer;
+let limit = 9;
 
-let winningCombination = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
-    [1,4,7],
-    [2,5,8],
-    [3,6,9],
-    [1,5,9],
-    [3,5,7]
-]
-
-function thresholdBroke(playerPick){
-    let count = 0;
-    for(let i = 0; i < gameBoard.length; i++){
-        if(gameBoard[i] == playerPick) count++;
-        if(count == 3) return true;
+// Player constructor
+function Player(name, pick){
+    if(!new.target){
+        throw new Error("Use the new keyword. This is a constructor ya idiot!!");
     }
 
-    return false;
+    this.name = name;
+    this.pick = pick;
+    this.hasPlayed = false;
+
+    this.madeMove = function(){
+        this.hasPlayed = true;
+    }
+
+    this.moveTime = function(){
+        this.hasPlayed = false;
+    }
+
+    this.getPick = function(){
+        return this.pick;
+    }
+
+    this.getName = function(){
+        return this.name;
+    }
 }
 
-function checkingForMatch(player){
-    let winner;
-    for(let i = 0; i < 8; i++){
-        let winning = 0;
-        for(let j = 0; j < 3; j++){
-            if(gameBoard[winningCombination[i][j]-1] == player) winning++;
-            if(winning == 3){
-                winner = player;
-                return winner;
+// GameBoard and mechanisms as IIFE
+let game = (function(){
+    let gameBoard = [
+        -1,-1,-1,
+        -1,-1,-1,
+        -1,-1,-1
+    ];
+    const winningCombination = [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9],
+        [1,4,7],
+        [2,5,8],
+        [3,6,9],
+        [1,5,9],
+        [3,5,7]
+    ];
+    const thresholdBroke = function(playerPick){
+        let count = 0;
+        for(let i = 0; i < gameBoard.length; i++){
+            if(gameBoard[i] == playerPick) count++;
+            if(count == 3) return true;
+        }
+    
+        return false;
+    }
+    const checkingForMatch = function(player){
+        let winner;
+        for(let i = 0; i < 8; i++){
+            let winning = 0;
+            for(let j = 0; j < 3; j++){
+                if(gameBoard[winningCombination[i][j]-1] == player) winning++;
+                if(winning == 3){
+                    winner = player;
+                    return winner;
+                }
             }
         }
-    }
-    return winner;
-}
+        return winner;
+    };
+
+    return {gameBoard, thresholdBroke, checkingForMatch};
+
+})();
 
 
+// Event function for every click
+let playGame = function(playerOne, playerTwo, index, game){
+    if(!winningPlayer & limit > 0){
+        let moveFor = playerOne.hasPlayed ? playerTwo : playerOne;
 
-
-    let playerOne = +(prompt("Pick your play. 0 or 1 ?"));
-    let playerTwo;
-
-    if(playerOne == 1) playerTwo = 0;
-    else playerTwo = 1;
-
-    alert(`Player one is ${playerOne}`);
-    alert(`Player two is ${playerTwo}`);
-
-
-    let winningPlayer;
-    let limit = 9;
-    while(!winningPlayer && limit > 0){
-
-        let playerOneMove = +(prompt("Whats your move playerOne?"));
-        console.log(playerOneMove);
-        gameBoard[playerOneMove-1] = playerOne;
+        // Making the move in the board
+        game.gameBoard[index] = moveFor.pick;
         limit--;
 
-        if(thresholdBroke(playerOne)){
-            winningPlayer = checkingForMatch(playerOne);
+        // Checking whether won
+        if(game.thresholdBroke(moveFor.pick)){
+            winningPlayer = game.checkingForMatch(moveFor.pick);
+            if(winningPlayer) showWinner(winningPlayer, playerOne, playerTwo);
         }
-
-
-        if(winningPlayer) break;
-
-        if(!winningPlayer){
-            let playerTwoMove = +(prompt("Whats your move playerTwo?"));
-
-            gameBoard[playerTwoMove-1] = playerTwo;
-            limit--;
-
-            if(thresholdBroke(playerTwo)){
-                winningPlayer = checkingForMatch(playerTwo);
-            }
-
-
-    }
+        
     }
 
+};
 
-if(limit == 0) winningPlayer = 'No one';
+function showWinner(winnerPick, playerOne, playerTwo){
+    let winnerDialog = document.querySelector('.winnerBoard');
+    let winnerMessage = document.querySelector('.winnerBoard.message')
+    let winner;
+    winner = playerOne.getPick() == winnerPick ? playerOne.getName() : playerTwo.getName();
+        if(limit == 0){
+            winnerMessage.innerText = "DRAW!!!!";
+            winnerDialog.showModal();
+        }
+        else{
+            winnerMessage.innerText = `${winner} wins!!!`;
+            winnerDialog.showModal();
+        }
+}
 
-alert(`result: ${winningPlayer}`);
